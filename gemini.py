25a409bb8e3376695c71453656232ed9cd373d36
OrlandoJE <orlandoesparza6@gmail.com>
2025-02-19 23:48:05 -0600

    Gemini running

--- gemini.py
+++ gemini.py
@@ -1,17 +1,22 @@
 import os
 from google import genai
 from google.genai import types
+from api_key import api_key
 
-client = genai.Client()
+client = genai.Client(api_key=api_key)
 
-myfile = client.files.upload(path='prueba.mp3')
+with open("media/prueba.mp3", "rb") as f:
+    image_bytes = f.read()
 
 response = client.models.generate_content(
-  model='gemini-2.0-flash',
-  contents=[
-    'Describe this audio clip',
-    myfile,
-  ]
+    model="gemini-2.0-flash",
+    contents=[
+        "Describe este audio, por favor",
+        types.Part.from_bytes(
+            data=image_bytes,
+            mime_type="audio/mp3",
+        ),
+    ],
 )
 
 print(response.text)
