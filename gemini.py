6795be80bf8b945512f1acacc854aa9eb480df2e
OrlandoJE <orlandoesparza6@gmail.com>
2025-03-05 12:56:27 -0600

    gemini integration

--- gemini.py
+++ gemini.py
@@ -5,18 +5,23 @@ from api_key import api_key
 
 client = genai.Client(api_key=api_key)
 
-with open("media/prueba.mp3", "rb") as f:
-    image_bytes = f.read()
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents=[
-        "Describe este audio, por favor",
-        types.Part.from_bytes(
-            data=image_bytes,
-            mime_type="audio/mp3",
-        ),
-    ],
-)
+def getImageDescription():
+    with open("media/prueba.mp3", "rb") as f:
+        image_bytes = f.read()
 
-print(response.text)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=[
+            "Describe este audio, por favor",
+            types.Part.from_bytes(
+                data=image_bytes,
+                mime_type="audio/mp3",
+            ),
+        ],
+    )
+
+    print(response.text)
+
+
+getImageDescription()
